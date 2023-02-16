0ad9f48c0761bdf82e881a581daf3f89cc4b547c
andrija55 <andrijaprotic55@gmail.com>
2023-02-16 19:27:14 +0100

    dodata stranica za login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './komponente/Navbar';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { useState } from 'react';
-
+import LoginPage from './komponente/LoginPage';
+import React from 'react';
 function App() {
   const[token,setToken] = useState(null);
 
@@ -12,7 +14,7 @@ function App() {
         <BrowserRouter  >
           <Navbar token={token} ></Navbar>
           <Routes>
-            <Route path="/" element={<Login addToken={addToken}></Login>}></Route>
+            <Route path="/" element={<LoginPage addToken={setToken}></LoginPage>}></Route>
              
            
           </Routes>
--- src/komponente/Navbar.jsx
+++ src/komponente/Navbar.jsx
@@ -1,9 +1,41 @@
  
 import { Link, useNavigate } from "react-router-dom";
+import axios from 'axios';
+import React from 'react';
 function Navbar({token}) {
-
+  const navigate = useNavigate();
+ 
+  function handleLogout(){ 
+   
+     
+    var config = {
+      method: 'post',
+      url: '/api/logout',
+      headers:{'Authorization': `Bearer ${ window.sessionStorage.getItem('auth_token')}`},
+    };
  
     
+    axios(config)
+    .then(function (response) {
+     
+      console.log(response);
+     
+      window.sessionStorage.setItem('auth_token',null); 
+      window.sessionStorage.setItem('auth_name',null); 
+      window.sessionStorage.setItem('auth_id',null); 
+      navigate('/');
+      sessionStorage.clear();
+      window.location.reload()
+
+    })
+    .catch(function (error) {
+     
+      
+      console.log(error);
+      
+
+    }); 
+  }
   return (
 
       <div className="navbar">
