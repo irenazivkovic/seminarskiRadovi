57737b98cf70629c4f192e9dc9a3e3d67c6a9862
irenazivkovic <zivkovicirena3@gmail.com>
2023-02-16 18:50:26 +0100

    autentifikacija i autorizacija i dodate api rute

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AutfController;
+use App\Http\Controllers\KomentarController;
+use App\Http\Controllers\RadController;
+use App\Http\Controllers\ZadatakController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +17,33 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get("/radovi",[RadController::class,'index']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post("/register",[AutfController::class,'register']);
+Route::post("/login",[AutfController::class,'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+
+ 
+
+    Route::post("/radovi",[RadController::class,'store']);
+
+    
+    Route::post("/logout",[AutfController::class,'logout']);
+
+});
+
+
+Route::middleware(['auth:sanctum','isAPIAdmin'])->group(function(){ //ako je ulogovan admin
+
+
+    Route::post("/komentar",[KomentarController::class,'store']);
+
+    Route::post("/zadatak",[ZadatakController::class,'store']);
+    Route::put("/zadatak/{id}",[ZadatakController::class,'update']);
+    Route::delete("/zadatak/{id}",[ZadatakController::class,'destroy']);
+    Route::post("/logout",[AutfController::class,'logout']);
+
+
+
 });
